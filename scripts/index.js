5b12f714fb7ad40fbb25af312bf3eb36c5156199
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-03-20 16:26:14 +0200

    functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
-const x = prompt("Количество часов:")
-const secondsInAMinute=60
-const minutesInAnHour=60
-const secondsInAnHour= secondsInAMinute * minutesInAnHour * x
-alert (secondsInAnHour)
+//1
+function numbers (number1, number2){
+    return number1 === number2 ? `true` : `false`
+}
+//2
+function nums (num1, num2){
+    return num1 + num2 > 10 ? `true` : `false`
+}
+//3
+function numbs (number){
+    return number < 0 ? `true` : `false`
+}
