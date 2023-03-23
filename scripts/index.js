3f8f2def71fe24df379e842bddbc747844e76527
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-03-23 14:20:45 +0200

    boolean

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,15 @@
 //1
 function numbers (number1, number2){
-    return number1 === number2 ? `true` : `false`
+    return number1 === number2;
 }
+console.log(numbers)
 //2
 function nums (num1, num2){
-    return num1 + num2 > 10 ? `true` : `false`
+    return num1 + num2 > 10;
 }
+console.log(nums)
 //3
 function numbs (number){
-    return number < 0 ? `true` : `false`
+    return number < 0;
 }
+console.log(numbs)
